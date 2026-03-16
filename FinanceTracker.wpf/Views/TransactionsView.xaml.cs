eb13d4760d1da05bb57d8704391cf1b140e4f724
TymurKashchuk <t1mu4337@gmail.com>
2026-03-16 21:26:57 +0200

    add context menu with delete action to transactions grid

--- FinanceTracker.wpf/Views/TransactionsView.xaml.cs
+++ FinanceTracker.wpf/Views/TransactionsView.xaml.cs
@@ -1,28 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using FinanceTracker.wpf.Models;
+using FinanceTracker.wpf.ViewModels;
 
 namespace FinanceTracker.wpf.Views
 {
-    /// <summary>
-    /// Interaction logic for TransactionsView.xaml
-    /// </summary>
     public partial class TransactionsView : UserControl
     {
         public TransactionsView()
         {
             InitializeComponent();
+        }
+
+        private void DeleteTransaction_Click(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is MainViewModel vm && TransactionsDataGrid.SelectedItem is Transaction t)
+            {
+                vm.DeleteTransactionCommand.Execute(t);
+            }
         }
     }
 }
